145ae7ff1767abb01b990226a77fbfb2a426ba99
Dxung <dung.lx183505@gmail.com>
2022-08-29 20:58:53 +0700

    update 29/8
    * Hoàn thành thiết kế map
    * Hoàn thành thiết kế pellet nhỏ

--- Project I/Assets/Scripts/Environment/WallGenerating.cs
+++ Project I/Assets/Scripts/Environment/WallGenerating.cs
@@ -6,8 +6,22 @@ using UnityEngine.Tilemaps;
 public class WallGenerating : MonoBehaviour
 {
     [SerializeField] private Tilemap _myTileMap;
-    [SerializeField] private TileBase _ruleTile;
+
+    [Header("TileBase that marks the tile to draw")]
+    [SerializeField] private TileBase _wallTile;
+    [SerializeField] private TileBase _blank;
+
+    [Header("The gameObject to instantiate")]
     [SerializeField] private GameObject _wallPreference;
+    [SerializeField] private GameObject _smallPellet;
+
+    [Header("check floating height of pellet")]
+    [SerializeField] private float _up;
+    [SerializeField] private int _numberOfPellet = 0;
+
+    
+    
+    
 
     private void Start()
     {
@@ -16,7 +30,11 @@ public class WallGenerating : MonoBehaviour
     // If the tile at that position is painted, Create the wallpreference at that space.
         foreach(Vector3Int position in _myTileMap.cellBounds.allPositionsWithin)
         {
-            if (_myTileMap.GetTile(position) == _ruleTile)
+            if(_myTileMap.GetTile(position) == _blank)
+            {
+             
+            }
+            else if (_myTileMap.GetTile(position) == _wallTile)
             {
 
                 // Convert tile position at that cell to world position 
@@ -25,7 +43,15 @@ public class WallGenerating : MonoBehaviour
                 //Instantiate the wallpreference at that world position
                 Instantiate(_wallPreference, cellPosition + new Vector3(0,0.5f,0), Quaternion.identity);
             }
+            else
+            {
+                Vector3 cellPosition = _myTileMap.GetCellCenterWorld(position);
+                Instantiate(_smallPellet, cellPosition + new Vector3(0, _up, 0), Quaternion.identity);
+                _numberOfPellet += 1;
+            }
+            
         }
     }
+
 
 }
---
+++ Project I/Assets/Scripts/Pellets/PelletFloating.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletFloating : MonoBehaviour
+{
+    private Vector3 _startPoint;
+    [SerializeField] private float _value;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _amplitude = 0.5f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Store starting position and get the random number for "_value" so each pellet will float uniquely
+        _startPoint = transform.position;
+        _value = Random.Range(0, 10.5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //to make the distance of floating change continuously (because Sin curve changes continuously) 
+        _value += Time.deltaTime;
+
+        //Sin range from -1 to 1, so pellet moving will never excess "max range" == "_amplitude"
+        transform.position = _startPoint + Vector3.up * Mathf.Sin(_value * _maxSpeed) * _amplitude;
+    }
+}
