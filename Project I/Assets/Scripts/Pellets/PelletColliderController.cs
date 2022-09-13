a3e875d791082f8eff6bd0db1d1d482c675a64d4
Dxung <dung.lx183505@gmail.com>
2022-09-13 18:21:01 +0700

    13/9/2022
    +fix bug
    +complete script to use when player touch pellets

---
+++ Project I/Assets/Scripts/Pellets/PelletColliderController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletColliderController : MonoBehaviour
+{
+    [Header("Pellet Particle System")]
+    private ParticleSystem _pelletParticleSystem;
+    private float _particleSystemDuration;
+
+    [Header("other references to use when particle effect happens")]
+    private Collider _pelletTrigger;
+    private MeshRenderer _pelletMesh;
+    private Light _pelletLight;
+
+
+    private void Awake()
+    {
+        Debug.Log(this.transform.parent.gameObject);
+
+        _pelletParticleSystem = this.transform.parent.GetComponentInChildren<ParticleSystem>();
+        _pelletMesh = this.transform.parent.GetComponent<MeshRenderer>();
+        _pelletTrigger = this.GetComponent<Collider>();
+        _pelletLight = this.transform.parent.GetComponentInChildren<Light>();
+
+        //get the time that particle system complete all circle
+        _particleSystemDuration = _pelletParticleSystem.duration + _pelletParticleSystem.startLifetime;
+
+    }
+    //this script will work when pellets be touched by player
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("da cham");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //turn off mesh, so you will not see the pellet remains on screen when particles come out
+            _pelletMesh.enabled = false;
+
+            //turn off light
+            _pelletLight.enabled = false;
+
+            //turn off trigger collider
+            _pelletTrigger.enabled = false;
+
+            //play particle effect
+            _pelletParticleSystem.Play();
+
+            Destroy(this.transform.parent.gameObject, _particleSystemDuration);
+
+            
+            
+        }
+    }
+}
--- Project I/Assets/Scripts/Player/FirstPersonController.cs
+++ Project I/Assets/Scripts/Player/FirstPersonController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class FirstPersonController : MonoBehaviour
 {
 
-
     public bool _canMove { get; private set; } = true;
 
     [Header("Movement Parameters")]
